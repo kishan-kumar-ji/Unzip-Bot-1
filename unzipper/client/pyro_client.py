92212ec2d45d0abddb63a37772431b2b4b6c9dce
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-04-15 20:06:42 +0530

    Update pyro_client.py

--- unzipper/client/pyro_client.py
+++ unzipper/client/pyro_client.py
@@ -19,7 +19,7 @@ from os import path, remove, stat
 from config import Config
 from pyrogram import Client
 from .caching import STRINGS
-from gofile2 import Async_Gofile
+from gofile2 import sync_Gofile
 from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, Message
 from unzipper.database.language import get_language
@@ -96,7 +96,7 @@ class UnzipperBot(Client):
                 # Uploads the file to gofile.io
                 upmsg = await self.send_message(c_id, text=STRINGS[lang]["alert_file_too_large"])
                 try:
-                    ga = Async_Gofile()
+                    ga = sync_Gofile()
                     gfio = await ga.upload(doc_f)
                     from unzipper import Buttons
                     await upmsg.edit("**Your file has been uploaded to gofile! Click on the below button to download it 👇**", reply_markup=await Buttons.make_button("Gofile link 🔗", url=gfio["downloadPage"]))
